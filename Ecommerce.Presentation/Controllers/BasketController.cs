d7443150750f4834ec1803566007e38d3fe07011
Mohamed <mhmdabdrabo2019@gmail.com>
2025-11-19 03:20:49 +0200

    Controller Dn

---
+++ Ecommerce.Presentation/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Service.Abstraction;
+using Ecommerce.Shared.BasketDTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController : Controller
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasketDTO>> GetBasket(string basketId)
+        {
+            var basket = await _basketService.GetBasketAsync(basketId);
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasketDTO>> CreateOrUpdateBasket(CustomerBasketDTO basket)
+        {
+            var Basket = await _basketService.CreateOrUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteBasket(string basketId)
+        {
+            var Basket = await _basketService.DeleteBasketAsync(basketId);
+            return Ok(Basket);
+        }
+    }
+}
